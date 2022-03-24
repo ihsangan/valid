916e02ed9cac438d3ccb2455e151c49f8d779769
Ihsan <mi598342@gmail.com>
2022-03-24 10:43:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -20,11 +20,12 @@ if (path.includes('/ml')) {
 		body
 	})
 	const response = await fetch(request)
-	const data = `${response.body}`
+	const data = response.body
 	//const par = JSON.parse(data)
         //const obj = new Object(data).toString()
 	//const user = par.result
-	return new Response(response.body, {
+	return new Response(data
+, {
 		status: 200,
 		headers: {
 			'Content-Type': 'application/json'
