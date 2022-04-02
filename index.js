0371e87db13f7785f6d1c904f28b8f01489b1faf
Ihsan <mi598342@gmail.com>
2022-04-02 16:41:01 +0800

    Final ver.

--- index.js
+++ index.js
@@ -5,51 +5,58 @@ async function handleRequest(request) {
   const url = new URL(request.url)
   const path = url.pathname
   const params = url.searchParams
-  const method = request.method
   const headers = request.headers
-if (path.includes('/ml')) {
-	const id = params.get('id')
-	const srv = params.get('server')
-	const endpoint = 'https://order-sg.codashop.com/initPayment.action'
-	const body = `voucherPricePoint.id=4150&voucherPricePoint.price=1565.0&voucherPricePoint.variablePrice=0&user.userId=${id}&user.zoneId=${srv}&msisdn=&voucherTypeName=MOBILE_LEGENDS&shopLang=id_ID`
-	const request = new Request(endpoint, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded'
-		},
-		body
-	})
-	const response = await fetch(request)
-	const data = await response.json()
-	return new Response(decodeURI(data.result), {
-		status: 200,
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	})
+  const endpoint = 'https://order-sg.codashop.com/initPayment.action'
+  const id = params.get('id')
+  const srv = params.get('server')
+  if (path.includes('/ml')) {
+    const body = `voucherPricePoint.id=4150&voucherPricePoint.price=1565.0&voucherPricePoint.variablePrice=0&user.userId=${id}&user.zoneId=${srv}&msisdn=&voucherTypeName=MOBILE_LEGENDS&shopLang=id_ID`
+    const request = new Request(endpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body
+    })
+    const response = await fetch(request)
+    const data = await response.json()
+    return new Response(decodeURIComponent(data.result), {
+      status: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET',
+        'Access-Control-Expose-Headers': '*',
+        'Content-Type': 'application/json; charset=utf-8'
+      }
+    })
+  }
+  if (path.includes('/ff')) {
+    const body = `voucherPricePoint.id=8050&voucherPricePoint.price=1000.0&voucherPricePoint.variablePrice=0&user.userId=${id}&voucherTypeName=FREEFIRE&shopLang=id_ID`
+    const request = new Request(endpoint, {
+      method: 'POST',
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET",
+        "Access-Control-Expose-Headers": "*",
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body
+    })
+    const response = await fetch(request)
+    const data = await response.json()
+    return new Response(decodeURIComponent(data.result), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      }
+    })
+  }
+  else {
+    return new Response(`{"success":false,"message":"Bad Request"}`, {
+      status: 400,
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      }
+    })
+  }
 }
-	if (path.includes('/ff')) {
-		const id = params.get('id')
-		const endpoint = 'https://order-sg.codashop.com/initPayment.action'
-		const body = `voucherPricePoint.id=8050&voucherPricePoint.price=1000.0&voucherPricePoint.variablePrice=0&user.userId=${id}&voucherTypeName=FREEFIRE&shopLang=id_ID`
-		const request = new Request(endpoint, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded'
-			},
-			body
-		})
-		const response = await fetch(request)
-		const data = JSON.stringify(response.body)
-		const result = JSON.parse(data)
-		const user = result.result
-		return new Response(user, {
-			status: 200,
-			headers: {
-				'Content-Type': 'text/plain'
-			}
-		})
-	}
-}
-
-
