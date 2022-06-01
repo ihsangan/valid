76b25094bdc92105a6c7295e52088686030f86a1
Ihsan <me@isan.eu.org>
2022-06-01 16:02:54 +0800

    pr

--- index.js
+++ index.js
@@ -9,115 +9,116 @@ async function callAPI(request) {
   const id = params.get('id')
   const zone = params.get('zone')
   try {
-  if (!id) {
-    return `{"success":false,"message":"Bad Request"}`
-  }
-  if (path.includes('/gi') && id.startsWith('6')) {
-    const body = `voucherPricePoint.id=116054&voucherPricePoint.price=16000.0&voucherPricePoint.variablePrice=0&user.userId=${id}&user.zoneId=os_usa&voucherTypeName=GENSHIN_IMPACT&shopLang=id_ID`
-    const request = new Request(endpoint, {
-      method: 'POST',
-      headers: {
-	'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body
-    })
-    const response = await fetch(request)
-    const data = await response.json()
-    let result = `{"success":true,"game":"Genshin Impact","server":"America","id":${id},"name":"${data.confirmationFields.username}"}`
-    return result
-  }
-  if (path.includes('/gi') && id.startsWith('7')) {
-    const body = `voucherPricePoint.id=116054&voucherPricePoint.price=16000.0&voucherPricePoint.variablePrice=0&user.userId=${id}&user.zoneId=os_euro&voucherTypeName=GENSHIN_IMPACT&shopLang=id_ID`
-    const request = new Request(endpoint, {
-      method: 'POST',
-      headers: {
-	'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body
-    })
-    const response = await fetch(request)
-    const data = await response.json()
-    let result = `{"success":true,"game":"Genshin Impact","server":"Europe","id":${id},"name":"${data.confirmationFields.username}"}`
-    return result
-  }
+    if (!id) {
+      return `{"success":false,"message":"Bad Request"}`
+    }
+    if (path.includes('/gi') && id.startsWith('6')) {
+      const body = `voucherPricePoint.id=116054&voucherPricePoint.price=16000.0&voucherPricePoint.variablePrice=0&user.userId=${id}&user.zoneId=os_usa&voucherTypeName=GENSHIN_IMPACT&shopLang=id_ID`
+      const request = new Request(endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body
+      })
+      const response = await fetch(request)
+      const data = await response.json()
+      let result = `{"success":true,"game":"Genshin Impact","server":"America","id":${id},"name":"${data.confirmationFields.username}"}`
+      return result
+    }
+    if (path.includes('/gi') && id.startsWith('7')) {
+      const body = `voucherPricePoint.id=116054&voucherPricePoint.price=16000.0&voucherPricePoint.variablePrice=0&user.userId=${id}&user.zoneId=os_euro&voucherTypeName=GENSHIN_IMPACT&shopLang=id_ID`
+      const request = new Request(endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body
+      })
+      const response = await fetch(request)
+      const data = await response.json()
+      let result = `{"success":true,"game":"Genshin Impact","server":"Europe","id":${id},"name":"${data.confirmationFields.username}"}`
+      return result
+    }
     if (path.includes('/gi') && id.startsWith('8')) {
-    const body = `voucherPricePoint.id=116054&voucherPricePoint.price=16000.0&voucherPricePoint.variablePrice=0&user.userId=${id}&user.zoneId=os_asia&voucherTypeName=GENSHIN_IMPACT&shopLang=id_ID`
-    const request = new Request(endpoint, {
-      method: 'POST',
-      headers: {
-	'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body
-    })
-    const response = await fetch(request)
-    const data = await response.json()
-    let result = `{"success":true,"game":"Genshin Impact","server":"Asia","id":${id},"name":"${data.confirmationFields.username}"}`
-    return result
+      const body = `voucherPricePoint.id=116054&voucherPricePoint.price=16000.0&voucherPricePoint.variablePrice=0&user.userId=${id}&user.zoneId=os_asia&voucherTypeName=GENSHIN_IMPACT&shopLang=id_ID`
+      const request = new Request(endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body
+      })
+      const response = await fetch(request)
+      const data = await response.json()
+      let result = `{"success":true,"game":"Genshin Impact","server":"Asia","id":${id},"name":"${data.confirmationFields.username}"}`
+      return result
     }
     if (path.includes('/gi') && id.startsWith('9')) {
-    const body = `voucherPricePoint.id=116054&voucherPricePoint.price=16000.0&voucherPricePoint.variablePrice=0&user.userId=${id}&user.zoneId=os_cht&voucherTypeName=GENSHIN_IMPACT&shopLang=id_ID`
-    const request = new Request(endpoint, {
-      method: 'POST',
-      headers: {
-	'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body
-    })
-    const response = await fetch(request)
-    const data = await response.json()
-    let result = `{"success":true,"game":"Genshin Impact","server":"SAR (Taiwan, Hong Kong, Macao)","id":${id},"name":"${data.confirmationFields.username}"}`
-    return result
+      const body = `voucherPricePoint.id=116054&voucherPricePoint.price=16000.0&voucherPricePoint.variablePrice=0&user.userId=${id}&user.zoneId=os_cht&voucherTypeName=GENSHIN_IMPACT&shopLang=id_ID`
+      const request = new Request(endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body
+      })
+      const response = await fetch(request)
+      const data = await response.json()
+      let result = `{"success":true,"game":"Genshin Impact","server":"SAR (Taiwan, Hong Kong, Macao)","id":${id},"name":"${data.confirmationFields.username}"}`
+      return result
     }
-  if (path.includes('/ml') && !zone) {
-    return `{"success":false,"message":"Bad Request"}`
+    if (path.includes('/ml') && !zone) {
+      return `{"success":false,"message":"Bad Request"}`
+    }
+    if (path.includes('/ml')) {
+      const body = `voucherPricePoint.id=4150&voucherPricePoint.price=1579.0&voucherPricePoint.variablePrice=0&user.userId=${id}&user.zoneId=${zone}&msisdn=&voucherTypeName=MOBILE_LEGENDS&shopLang=id_ID`
+      const request = new Request(endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body
+      })
+      const response = await fetch(request)
+      const data = await response.json()
+      let result = `{"success":true,"game":"Mobile Legends: Bang Bang","id":${id},"zoneId":${zone},"name":"${data.confirmationFields.username}"}`
+      return result
+    }
+    if (path.includes('/ff')) {
+      const body = `voucherPricePoint.id=8050&voucherPricePoint.price=1000.0&voucherPricePoint.variablePrice=0&user.userId=${id}&voucherTypeName=FREEFIRE&shopLang=id_ID`
+      const request = new Request(endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body
+      })
+      const response = await fetch(request)
+      const data = await response.json()
+      let result = `{"success":true,"game":"Garena Free Fire","id":${id},"name":"${data.confirmationFields.roles[0].role}"}`
+      return result
+    }
+    if (path.includes('/aov')) {
+      const body = `voucherPricePoint.id=7946&voucherPricePoint.price=10000.0&voucherPricePoint.variablePrice=0&user.userId=${id}&voucherTypeName=AOV&shopLang=id_ID`
+      const request = new Request(endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body
+      })
+      const response = await fetch(request)
+      const data = await response.json()
+      let result = `{"success":true,"game":"Garena AOV (Arena Of Valor)","id":${id},"name":"${data.confirmationFields.roles[0].role}"}`
+      return result
+    }
+    else {
+      let result = `{"success":false,"message":"Bad request"}`
+      return result
+    }
   }
-  if (path.includes('/ml')) {
-    const body = `voucherPricePoint.id=4150&voucherPricePoint.price=1579.0&voucherPricePoint.variablePrice=0&user.userId=${id}&user.zoneId=${zone}&msisdn=&voucherTypeName=MOBILE_LEGENDS&shopLang=id_ID`
-    const request = new Request(endpoint, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body
-    })
-    const response = await fetch(request)
-    const data = await response.json()
-    let result = `{"success":true,"game":"Mobile Legends: Bang Bang","id":${id},"zoneId":${zone},"name":"${data.confirmationFields.username}"}`
-    return result
-  }
-  if (path.includes('/ff')) {
-    const body = `voucherPricePoint.id=8050&voucherPricePoint.price=1000.0&voucherPricePoint.variablePrice=0&user.userId=${id}&voucherTypeName=FREEFIRE&shopLang=id_ID`
-    const request = new Request(endpoint, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body
-    })
-    const response = await fetch(request)
-    const data = await response.json()
-    let result = `{"success":true,"game":"Garena Free Fire","id":${id},"name":"${data.confirmationFields.roles[0].role}"}`
-    return result
-  }
-  if (path.includes('/aov')) {
-    const body = `voucherPricePoint.id=7946&voucherPricePoint.price=10000.0&voucherPricePoint.variablePrice=0&user.userId=${id}&voucherTypeName=AOV&shopLang=id_ID`
-    const request = new Request(endpoint, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body
-    })
-    const response = await fetch(request)
-    const data = await response.json()
-    let result = `{"success":true,"game":"Garena AOV (Arena Of Valor)","id":${id},"name":"${data.confirmationFields.roles[0].role}"}`
-    return result
-  }
-  else {
-    let result = `{"success":false,"message":"Bad request"}`
-    return result
-  }
-} catch (error) {
+  catch (error) {
     let result = `{"success":false,"message":"Cannot find nickname from your request."}`
     return result
   }
@@ -145,7 +146,9 @@ async function serveResult(request) {
 }
 async function checkCache(request) {
   if (request.method !== 'GET') {
-    return new Response('', { status: 405 })
+    return new Response(`{"success":false,"message":"Method not allowed"}`, {
+      status: 405
+    })
   }
   let now = Date.now()
   let cache = caches.default
