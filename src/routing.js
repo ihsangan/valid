92080c1b71c72a96eab8b0e9c94ca668601ea5f8
ihsangan <me@isan.eu.org>
2024-06-23 11:06:44 +0800

    setup coc

--- src/routing.js
+++ src/routing.js
@@ -1,6 +1,7 @@
 import { getUrl, endpoint } from './utils'
 import ff from './router/ff'
 import ml from './router/ml'
+import coc from './router/coc'
 
 export default async function callAPI(request) {
   const url = getUrl(request)
@@ -15,6 +16,8 @@ export default async function callAPI(request) {
       return await ff(id)
     } if (path.includes('/ml')) {
       return await ml(id, zone)
+    } if (path.includes('/coc')) {
+      return await coc(id)
     } else {
       return { success: false, message: 'Bad request' }
     }
