df0b56bfa32c89c1812e71823799c1761e4eac47
ihsangan <me@isan.eu.org>
2024-06-23 10:27:42 +0800

    setup ml

--- src/routing.js
+++ src/routing.js
@@ -1,5 +1,6 @@
 import { getUrl, endpoint } from './utils'
 import ff from './router/ff'
+import ml from './router/ml'
 
 export default async function callAPI(request) {
   const url = getUrl(request)
@@ -12,6 +13,8 @@ export default async function callAPI(request) {
       return { success: false, message: 'Bad Request' }
     } if (path.includes('/ff')) {
       return await ff(id)
+    } if (path.includes('/ml')) {
+      return await ml(id, zone)
     } else {
       return { success: false, message: 'Bad request' }
     }
