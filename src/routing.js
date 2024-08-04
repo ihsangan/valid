4e30a626ea3c21e50b37b7b2cf56210f5835235a
ihsangan <me@isan.eu.org>
2024-08-04 10:25:56 +0800

    indexing route

--- src/routing.js
+++ src/routing.js
@@ -1,57 +1,61 @@
-import { getUrl } from './utils'
-import ff from './router/ff'
-import ml from './router/ml'
-import gi from './router/gi'
-import hi from './router/hi'
-import hsr from './router/hsr'
-import pb from './router/pb'
-import sm from './router/sm'
-import sus from './router/sus'
-import aov from './router/aov'
-import cod from './router/cod'
-import valo from './router/valo'
-import pgr from './router/pgr'
-import la from './router/la'
+import { getUrl } from './utils';
+import * as router from './router';
 
 export default async function callAPI(request) {
-  const url = getUrl(request)
-  const path = url.pathname
-  const params = url.searchParams
-  let id = params.get('id')
-  let zone = params.get('zone')
+  const url = getUrl(request);
+  const path = url.pathname;
+  const params = url.searchParams;
+  let id = params.get('id');
+  let zone = params.get('zone');
+
   try {
     if (!id) {
-      return { success: false, message: 'Bad request' }
-    } if (path.includes('/ff')) {
-      return await ff(id)
-    } if (path.includes('/ml')) {
-      return await ml(id, zone)
-    } if (path.includes('/gi')) {
-      return await gi(id)
-    } if (path.includes('/hi')) {
-      return await hi(id)
-    } if (path.includes('/hsr')) {
-      return await hsr(id)
-    } if (path.includes('/pb')) {
-      return await pb(id)
-    } if (path.includes('/sm')) {
-      return await sm(id)
-    }  if (path.includes('/sus')) {
-      return await sus(id)
-    } if (path.includes('/aov')) {
-      return await aov(id)
-    } if (path.includes('/cod')) {
-      return await cod(id)
-    } if (path.includes('/valo')) {
-      return await valo(id)
-    } if (path.includes('/pgr')) {
-      return await pgr(id, zone)
-    } if (path.includes('/la')) {
-      return await la(id, zone)
-    } else {
-      return { success: false, message: 'Bad request' }
+      return { success: false, message: 'Bad request' };
+    }
+
+    if (path.includes('/ff')) {
+      return await router.ff(id);
+    } 
+    if (path.includes('/ml')) {
+      return await router.ml(id, zone);
+    } 
+    if (path.includes('/gi')) {
+      return await router.gi(id);
+    } 
+    if (path.includes('/hi')) {
+      return await router.hi(id);
+    } 
+    if (path.includes('/hsr')) {
+      return await router.hsr(id);
+    } 
+    if (path.includes('/pb')) {
+      return await router.pb(id);
+    } 
+    if (path.includes('/sm')) {
+      return await router.sm(id);
+    }  
+    if (path.includes('/sus')) {
+      return await router.sus(id);
+    } 
+    if (path.includes('/aov')) {
+      return await router.aov(id);
+    } 
+    if (path.includes('/cod')) {
+      return await router.cod(id);
+    } 
+    if (path.includes('/valo')) {
+      return await router.valo(id);
+    } 
+    if (path.includes('/pgr')) {
+      return await router.pgr(id, zone);
+    } 
+    if (path.includes('/la')) {
+      return await router.la(id, zone);
+    } 
+    else {
+      return { success: false, message: 'Bad request' };
     }
   } catch (error) {
-    return { success: false, message: 'Not found' }
+    return { success: false, message: 'Not found' };
   }
 }
