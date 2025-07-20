7fc61765e4b08b6755e18b8f5176201968b43f84
ihsangan <me@isan.eu.org>
2025-07-20 17:24:11 +0800

    fix ff

--- src/router/ff.ts
+++ src/router/ff.ts
@@ -1,7 +1,7 @@
 import { hitCoda, Result } from '../utils'
 
 export default async function ff(id: number): Promise<Result> {
-  const body = `voucherPricePoint.id=8050&voucherPricePoint.price=1000&voucherPricePoint.variablePrice=0&user.userId=${id}&voucherTypeName=FREEFIRE&shopLang=id_ID`
+  const body = `voucherPricePoint.id=1032429&voucherPricePoint.price=1110&voucherPricePoint.variablePrice=0&user.userId=${id}&voucherTypeName=FREEFIRE&shopLang=id_ID`
   const data = await hitCoda(body)
   return {
     success: true,
