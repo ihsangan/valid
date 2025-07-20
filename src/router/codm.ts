f1ae01e1d72682024bbf3d9aa72246455e38810c
ihsangan <me@isan.eu.org>
2025-07-20 17:31:26 +0800

    update codm

--- src/router/codm.ts
+++ src/router/codm.ts
@@ -1,7 +1,7 @@
 import { hitCoda, Result } from '../utils'
 
 export default async function codm(id: number): Promise<Result> {
-  const body = `voucherPricePoint.id=46114&voucherPricePoint.price=5000&voucherPricePoint.variablePrice=0&user.userId=${id}&voucherTypeName=CALL_OF_DUTY&shopLang=id_ID&voucherTypeId=1&gvtId=90`
+  const body = `voucherPricePoint.id=1032849&voucherPricePoint.price=11100&voucherPricePoint.variablePrice=0&user.userId=${id}&voucherTypeName=CALL_OF_DUTY&shopLang=id_ID&voucherTypeId=1&gvtId=90`
   const data = await hitCoda(body)
   return {
     success: true,
