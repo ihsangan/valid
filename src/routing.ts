8c8748bca7d1681258444da3c2783d3ffa32ff54
ihsangan <me@isan.eu.org>
2025-10-16 18:53:27 +0800

    update routing.ts

--- src/routing.ts
+++ src/routing.ts
@@ -11,7 +11,7 @@ export default async function callAPI(url: string): Promise<Result> {
   if (!params.id) {
     return {
       success: false,
-      message: 'Bad request' 
+      message: params
     }
   }
   try {
