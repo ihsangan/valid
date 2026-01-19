d246848bb4feeab1a0193e16035a25ade8265f39
ihsangan <me@isan.eu.org>
2026-01-20 06:31:44 +0800

    update ff

--- src/router/ff.ts
+++ src/router/ff.ts
@@ -1,12 +1,12 @@
 import { hitCoda, Result } from '../utils'
 
 export default async function ff(id: number): Promise<Result> {
-  const body = `voucherPricePoint.id=8063&voucherPricePoint.price=10000&voucherPricePoint.variablePrice=0&user.userId=${id}&voucherTypeName=FREEFIRE&shopLang=id_ID`
+  const body = `voucherPricePoint.id=1032492&voucherPricePoint.price=11100&voucherPricePoint.variablePrice=0&user.userId=${id}&voucherTypeName=FREE_FIRE&shopLang=id_ID`
   const data = await hitCoda(body)
   return {
     success: true,
     game: 'Garena Free Fire',
     id,
-    name: data.confirmationFields.roles[0].role
+    name: data.confirmationFields.username
   }
 }
