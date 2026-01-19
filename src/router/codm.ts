7e151652f8fcf64c77d3fcc4cec31de9b0765aab
ihsangan <me@isan.eu.org>
2026-01-20 06:53:09 +0800

    update codm

--- src/router/codm.ts
+++ src/router/codm.ts
@@ -1,12 +1,12 @@
 import { hitCoda, Result } from '../utils'
 
 export default async function codm(id: number): Promise<Result> {
-  const body = `voucherPricePoint.id=46129&voucherPricePoint.price=10000&voucherPricePoint.variablePrice=0&user.userId=${id}&voucherTypeName=CALL_OF_DUTY&shopLang=id_ID&voucherTypeId=1&gvtId=90`
+  const body = `voucherPricePoint.id=1032849&voucherPricePoint.price=11100&voucherPricePoint.variablePrice=0&user.userId=${id}&voucherTypeName=CALL_OF_DUTY%3AMOBILE&shopLang=id_ID`
   const data = await hitCoda(body)
   return {
     success: true,
     game: 'Call Of Duty Mobile',
     id,
-    name: data.confirmationFields.roles[0].role
+    name: data.confirmationFields.username
   }
 }
