76990cea5cbbbcfbad6d9b43fb157f1c1572aa79
ihsangan <me@isan.eu.org>
2026-01-20 06:38:37 +0800

    update aov

--- src/router/aov.ts
+++ src/router/aov.ts
@@ -1,12 +1,12 @@
 import { hitCoda, Result } from '../utils'
 
 export default async function aov(id: number): Promise<Result> {
-  const body = `voucherPricePoint.id=7946&voucherPricePoint.price=10000&voucherPricePoint.variablePrice=0&user.userId=${id}&voucherTypeName=AOV&shopLang=id_ID&voucherTypeId=1&gvtId=32`
+  const body = `voucherPricePoint.id=1031547&voucherPricePoint.price=11100&voucherPricePoint.variablePrice=0&user.userId=${id}&voucherTypeName=ARENA_OF_VALOR&shopLang=id_ID`
   const data = await hitCoda(body)
   return {
     success: true,
     game: 'Garena: AOV (Arena Of Valor)',
     id,
-    name: data.confirmationFields.roles[0].role
+    name: data.confirmationFields.username
   }
 }
