e16afb8ad5f730f33e33dc09f461033fc773aba4
ihsangan <me@isan.eu.org>
2026-02-12 15:50:44 +0800

    update ml.ts

--- src/router/ml.ts
+++ src/router/ml.ts
@@ -17,12 +17,12 @@ export default async function ml(user_id: string, zone_id: string): Promise<Resu
     body: `{"shop_code":"MOBILE_LEGENDS","data":{"user_id":"1007909047","zone_id":"13044"}}`
   })
   //const result = await fetch(request)
-  const data = await result.json()
+  //const data = await result.json()
   return {
     success: true,
     game: 'Mobile Legends: Bang Bang',
     id,
     server: zone,
-    name: data
+    name: result.body
   }
 }
